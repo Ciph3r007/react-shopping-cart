57dd8ff362a731162f52a235ad6a06d17126b38d
Ciph3r007 <79519082+Ciph3r007@users.noreply.github.com>
2021-09-16 16:27:36 +0600

    Add reset button, refactor to use single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 class Counter extends React.Component {
-  state = { value: this.props.value };
-
-  handleIncrement = () => this.setState({ value: this.state.value + 1 });
-
   render() {
-    console.log(this.props);
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -23,13 +23,14 @@ class Counter extends React.Component {
 
   getBadgeClass() {
     let badgeClass = "badge m-2 bg-";
-    badgeClass += this.state.value === 0 ? "warning text-dark" : "primary";
+    badgeClass +=
+      this.props.counter.value === 0 ? "warning text-dark" : "primary";
 
     return badgeClass;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,21 +5,54 @@ class Counters extends React.Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 1 },
-      { id: 3, value: 2 },
-      { id: 4, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
     ],
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
+  // Refactored here to use single source of truth
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (id) => {
+    const counters = this.state.counters.filter((c) => c.id !== id);
+    this.setState({ counters });
   };
 
   render() {
     return (
-      <div>
-        {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value}>
-            <h4>Counter #{counter.id}</h4>
-          </Counter> // these properties are set as key of Counter.props [except `key`]
-        ))}
-      </div>
+      <>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        <div>
+          {this.state.counters.map((counter) => (
+            <Counter
+              key={counter.id}
+              counter={counter}
+              onIncrement={this.handleIncrement}
+              onDelete={this.handleDelete}
+            ></Counter> // these properties get set as key of Counter.props [except `key`]
+          ))}
+        </div>
+      </>
     );
   }
 }
